3b3aca0ebbfc39accef46355bcad669d928a8dd6
khd1234 <86230448+khd1234@users.noreply.github.com>
2026-04-10 08:21:36 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        small = float('inf')
+        max_diff = 0
+        for i in prices:
+            small = min(small, i)
+            max_diff = max(max_diff, i - small)
+        return int(max_diff)
+
+        
